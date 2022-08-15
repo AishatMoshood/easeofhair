fba92acc7db515925fc6e57f7c566e2112375019
AishatMoshood <aishatmoshood1@gmail.com>
2022-08-15 15:24:05 +0100

    setup modified

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 '''File starts the server'''
-from easetestprojapp import app, manager
+from easetestprojapp import app
 
 if __name__ == '__main__':
     app.run(debug=True)
-    manager.run()
+
 
     
