de9ff70f166e468c872a0f618bec8d1ad316c676
AishatMoshood <aishatmoshood1@gmail.com>
2022-08-15 13:14:07 +0100

    Error logger added to init

--- easetestprojapp/__init__.py
+++ easetestprojapp/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import sys, logging
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_mail import Mail, Message
 
 app = Flask(__name__, instance_relative_config=True)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 csrf = CSRFProtect(app)
 
 from easetestprojapp import config
