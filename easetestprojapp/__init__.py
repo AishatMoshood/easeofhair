a439175efe7e3a42bbf7ef4ee750ff52b596e1c5
AishatMoshood <aishatmoshood1@gmail.com>
2022-08-16 07:39:06 +0100

    Name attr added to enum for postgres

--- easetestprojapp/__init__.py
+++ easetestprojapp/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 import sys, logging
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from flask_migrate import Migrate
 from flask_mail import Mail, Message
 
 app = Flask(__name__, instance_relative_config=True)
@@ -14,7 +15,7 @@ app.config.from_object(config.ProductionConfig)
 app.config.from_pyfile('config.py', silent=False)
 
 db = SQLAlchemy(app)
-
+migrate = Migrate(app, db)
 mail = Mail(app)
 
 from easetestprojapp.myroutes import adminroutes, userroutes, salonroutes
